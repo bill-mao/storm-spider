3ea7881eedc4bd8ec1fe928a36cc1e5feefab149
Bill Mao <mao_bill@163.com>
2017-07-14 14:15:35 +0800

    能够jar，运行，但是storm UI 不能显示任务

---
+++ CrawlerTopology.java
@@ -0,0 +1,45 @@
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import bolt.FetchTemplateHtmlUrlsBolt;
+import bolt.FetchWebsiteBolt;
+import spout.FetchTemplateSpout;
+
+/**
+ * Created by coral on 2017/7/11.
+ */
+public class CrawlerTopology {
+
+
+    public static void main(String args[]) throws Exception{
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("FetchTemplate", new FetchTemplateSpout(), 1);
+        builder.setBolt("FetchTemplateHtmlUrls", new FetchTemplateHtmlUrlsBolt(), 2)
+                .localOrShuffleGrouping("FetchTemplate");
+        builder.setBolt("FetchWebsite", new FetchWebsiteBolt(), 5)
+                .localOrShuffleGrouping("FetchTemplateHtmlUrls");
+
+        Config conf = new Config();
+        // TODO: 2017/7/14  
+        conf.setDebug(true);
+
+        if(args == null && args.length >0){
+
+            conf.setNumWorkers(20);
+            conf.setMaxSpoutPending(5000);//?
+            conf.setMessageTimeoutSecs(120);
+//            conf.setNumAckers();
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }else{
+            conf.setMaxTaskParallelism(8);
+//            conf.setMaxSpoutPending(2);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("CrawlerTopology", conf, builder.createTopology());
+            //don't shutdown automatically
+        }
+    }
+
+
+
+}
